662b2b2ae8d7a23d36f2c6f3784300e84f6b13f3
mohamad olleik <mhmd.olleik17@gmail.com>
2024-08-09 22:25:24 +0300

    c2

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 fetch('https://trainingapi-wuay.onrender.com/data')
       .then(response => response.json())
-      .then(json => console.log(json))
+      .then(json => {
+        let users=document.getElementById('users');
+        json.forEach(element => {
+            let user=document.createElement('option');
+            user.innerHTML=element;
+            users.appendChild(user);
+
+            
+        });
+      })
 
       alert('full stack training')
